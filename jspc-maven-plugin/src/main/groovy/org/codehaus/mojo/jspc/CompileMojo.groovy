a346b626db82c5a22ca27ebd8c662bdb63043d8b
shinsuke <shinsuke@52ab4f32-60fc-0310-b215-8acea882cd1b>
2012-10-11 22:10:17 +0000

    MJSPC-21 created/add a dummy jar to process tld files.
    git-svn-id: http://svn.codehaus.org/mojo/trunk/mojo/jspc@17678 52ab4f32-60fc-0310-b215-8acea882cd1b

--- jspc-maven-plugin/src/main/groovy/org/codehaus/mojo/jspc/CompileMojo.groovy
+++ jspc-maven-plugin/src/main/groovy/org/codehaus/mojo/jspc/CompileMojo.groovy
@@ -19,6 +19,16 @@
 
 package org.codehaus.mojo.jspc
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+import java.util.jar.JarEntry;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 /**
  * Compile JSPs.
  *
@@ -40,6 +50,74 @@ class CompileMojo
     private List classpathElements
 
     protected List getClasspathElements() {
-        return classpathElements
+        def List list = new ArrayList()
+        def boolean tldExists = false
+        def String[] tlds = ["tld"]
+        def File tempJarDir = File.createTempFile("jscp-", "")
+        tempJarDir.delete()
+        tempJarDir.mkdir()
+        
+        try{
+            for (target in classpathElements){
+                File file = new File(target)
+                if(file.isFile()){
+                    list << target
+                } else if(file.isDirectory()){
+                    Collection tldFiles = FileUtils.listFiles(file, tlds, true)
+                    if(!tldFiles.isEmpty()){
+                        FileUtils.copyDirectory(file, tempJarDir)
+                        tldExists = true
+                    }
+                }
+            }
+
+            if(log.debugEnabled) {
+                log.debug("tldExists: ${tldExists}")
+            }
+
+            if(tldExists){
+                def File tempJarFile = File.createTempFile("jscptld-", ".jar")
+                tempJarFile.deleteOnExit()
+                createJarArchive(tempJarFile, tempJarDir)
+                list << tempJarFile
+            }
+        } finally {
+            FileUtils.deleteDirectory(tempJarDir)
+        }
+        return list
+    }
+    
+    protected void createJarArchive(File archiveFile, File tempJarDir) {
+        JarOutputStream jos = null;
+        try {
+            jos = new JarOutputStream(new FileOutputStream(archiveFile), new Manifest());
+  
+            def pathLength = tempJarDir.getAbsolutePath().length() + 1
+            def Collection files = FileUtils.listFiles(tempJarDir, null, true)
+            for (int i = 0; i < files.size; i++) {
+                if (!files[i].isFile()){
+                    continue
+                }
+
+                if(log.debugEnabled) {
+                    log.debug("file: " + files[i].getAbsolutePath())
+                }
+                
+                // Add entry
+                def name = files[i].getAbsolutePath().substring(pathLength)
+                JarEntry jarFile = new JarEntry(name);
+                jos.putNextEntry(jarFile);
+  
+                FileInputStream fis = null
+                try {
+                    fis = new FileInputStream(files[i]);
+                    IOUtils.copy(fis, jos)
+                } finally {
+                    IOUtils.closeQuietly(fis)
+                }
+            }
+        } finally {
+            IOUtils.closeQuietly(jos)
+        }
     }
 }
